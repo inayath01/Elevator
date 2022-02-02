d2976e55915dd46d2f45e8125645f743bf47529b
inayath01 <96566781+inayath01@users.noreply.github.com>
2022-02-02 11:44:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ from datetime import datetime
 app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] ={
-    'db': 'elevator',
-    'host': 'localhost',
-    'port' :  '27017',
+    'HOSTNAME': '165.232.186.203',
+    'PORTNAME' :  '27017',
+    'DB_NAME': 'elevator',
+    'COLLECTION_NAME' : 'elevator details',
 }
 
 
 @app.route("/")
 def query_records():
-    return render_template("table.html")
+    return render_template("table.html", utc_dt=datetime.datetime.utcnow())
 
 if __name__ == '__main__':
     app.debug = True
