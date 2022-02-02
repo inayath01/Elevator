dfa70f1cd0f9669decffd37cd1520394b102698e
inayath01 <96566781+inayath01@users.noreply.github.com>
2022-02-02 10:39:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template
-import datetime
+from flask_mongoengine import MongoEngine
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['MONGODB_SETTINGS'] ={
+    'db': 'elevator',
+    'host': 'localhost',
+    'port' :  '27017',
+}
+
+
 @app.route("/")
-def table():
-    return render_template("table.html", utc_dt=datetime.datetime.utcnow())
+def query_records():
+    return render_template("table.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    
