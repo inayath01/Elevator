61ab5e5996e8ed2612e6d5d250e67c8c4267d589
inayath01 <96566781+inayath01@users.noreply.github.com>
2022-02-03 17:21:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,36 @@
-from flask import Flask, render_template
-from flask_mongoengine import MongoEngine
-from datetime import datetime
+from flask import Flask, render_template,request,redirect,url_for 
+from bson import ObjectId 
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+title = "Elevator Details"
+heading = "Elevator Details"
 
-app.config['MONGODB_SETTINGS'] ={
-    'HOSTNAME': '165.232.186.203',
-    'PORTNAME' :  '27017',
-    'DB_NAME': 'elevator',
-    'COLLECTION_NAME' : 'elevator details',
-}
+client = MongoClient("mongodb://127.0.0.1:27017") 
+db = client.elevator    
+elevator = db.elevatordetails 
 
 
-@app.route("/")
-def query_records():
-    return render_template("table.html", utc_dt=datetime.datetime.utcnow())
+@app.route("/", methods=['POST'])
+def action ():
+	Sl.no=request.values.get("Sl.no")
+	Deviceid=request.values.get("Device id")
+	Buttonid=request.values.get("Button id")
+	Timestamp=request.values.get("Timestamp")
+	return redirect("/")
 
-if __name__ == '__main__':
-    app.debug = True
-    
+
+@app.route("/", methods=['GET'])
+def search():
+	key=request.values.get("key")
+	refer=request.values.get("refer")
+	if(key=="_id"):
+		elevator_l = elevator.find({refer:ObjectId(key)})
+	else:
+		elevator_l = elevator.find({refer:key})
+	return render_template('home.html',elevator=elevator_l,t=title,h=heading)
+
+if __name__ == "__main__":
+
+    app.run()
